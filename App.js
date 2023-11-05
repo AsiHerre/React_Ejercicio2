7935390f6f94c872e9a8c7fe75bb287e19e23ebd
AsiHerre <ikddl@plaiaundi.net>
2023-11-05 16:04:44 +0100

    Version 0: Proyecto creado para el ejercicio de las criptomonedas

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+/*
+git init
+git status
+git add .
+git commit -m ""
+git log 00_00
+git tag
+git push origin master
+git push --tags
+*/
 export default function App() {
   return (
     <View style={styles.container}>
